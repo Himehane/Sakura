e0f442e2b369cca46a26f07f2821c32b4cf3931b
Himehane <370252030@qq.com>
2019-01-06 16:44:28 +0800

    优化模型加载方式

--- footer.php
+++ footer.php
@@ -89,6 +89,7 @@
 	<canvas id="live2d" class="live2d"></canvas>
 </div>
 <script type="text/javascript" src="https://himehane.club/wp-content/plugins/live2d/loadModel.js"></script>
+<script>loadModel(modelPath)</script>
 <div class="changeSkin-gear no-select">
     <div class="keys">
         <span id="open-skinMenu">
